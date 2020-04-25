73c945d4440abec365bbeaa9e23ab1f039655619
Kimaru thagana <macbuntu@localhost.localdomain>
2020-04-25 17:35:04 +0300

    code comments added

--- dashboard.py
+++ dashboard.py
@@ -4,12 +4,14 @@ from PIL import Image
 
 st.sidebar.markdown("Welcome to Picture-lytics.")
 st.sidebar.selectbox("Link to the relevant datasets.", ["link 1","link 2"])# kaggle links to dataset
-page = st.sidebar.selectbox("Choose task", ["Plant Disease Classification", "Brain Tumor MRI Classification"])
+page = st.sidebar.selectbox("Choose task", ["Plant Disease Classification", "Brain Tumor MRI Classification"])# pages
 
 if page == "Plant Disease Classification":
+
     st.title("Image Classification with Google's Teachable Machine")
     st.header("Plant Disease Classification Example")
     st.write("Upload plant images")
+    # file upload and handling logic
     uploaded_file = st.file_uploader("Choose a brain MRI ...", type="jpg")
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
@@ -20,10 +22,11 @@ if page == "Plant Disease Classification":
         st.write("")
 
 elif page == "Brain Tumor MRI Classification":
+
     st.title("Image Classification with Google's Teachable Machine")
     st.header("Brain Tumor MRI Classification Example")
     st.text("Upload a brain MRI Image for image classification as tumor or no-tumor")
-
+    # file upload and handling logic
     uploaded_file = st.file_uploader("Choose a brain MRI ...", type="jpg")
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
